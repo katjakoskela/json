403a2159735a2fe1f5d08ba86c9a3d1db9986515
katjakoskela <159912923+katjakoskela@users.noreply.github.com>
2024-11-22 10:34:39 +0200

    js objektitaulukko

--- etusivu.js
+++ etusivu.js
@@ -24,12 +24,22 @@ function kerro (data){
     teksti = teksti + "<p>" + data.kuvaus + "</p>";
     teksti = teksti + "<p><img src='" + data.kuva + "' alt='kuva' ></p>";
     teksti = teksti + "<h3>Opintojakso: " + data.opintojakso.nimi + " " + data.opintojakso.tunnus + " " + data.opintojakso.opintopisteet + "op" + "</h3>";
-//taulukko
+
+    //taulukko
     teksti = teksti + "<ul>"
-for(var i = 0; i < data.sisalto.length; i++) {
-teksti = teksti + "<li>" + data.sisalto[i] + "</li>";
-}
-teksti = teksti + "</ul>"
+    // tauukon käsittely for-elementissä
+    for(var i = 0; i < data.sisalto.length; i++) {
+        teksti = teksti + "<li>" + data.sisalto[i] + "</li>";
+    }
+    //listaelementti kiinni
+    teksti = teksti + "</ul>"
+
+    //objektitaulukko
+    teksti = teksti + "<h3>Linkit</h3><ul>";
+    for(var i =0; i<data.tekniikat.lenght; i++){
+        teksti = teksti + "<li>" + data.tekniikat[i].aihe + "<a href='data.tekniikat[i].linkki'>" + + data.tekniikat[i].linkki + "</a></li>";
+    }
+
 
 //tulostus sivulle
 document.getElementById("vastaus").innerHTML = teksti;
