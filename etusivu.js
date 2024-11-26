19d6b8997ab9a009d6cd4a23ec2902cded7f51c7
katjakoskela <159912923+katjakoskela@users.noreply.github.com>
2024-11-26 16:47:44 +0200

    etusivu.js linkit

--- etusivu.js
+++ etusivu.js
@@ -1,46 +1,51 @@
 fetch('https://jaakkola.github.io/json/digitekniikat.json')
+    // Muunnetaan vastaus JSON-muotoon
+    .then(function (response) {
+        return response.json();
+    })
+    // Käsitellään muunnettu (eli JSON-muotoinen) vastaus
+    .then(function (responseJson) {
+        // Kutsutaan funktiota ja välitetään sille JSON-vastaus
+        kerro(responseJson);
+    })
+    // Jos tuli jokin virhe
+    .catch(function (error) {
+        document.getElementById("vastaus").innerHTML =
+            "<p>Tietoa ei pystytä hakemaan</p>";
+    });
 
-// Muunnetaan vastaus JSON muotoon
-.then(function (response) {
-return response.json();
-})
-
-// Käsitellään muunnettu (eli JSON muotoinen) vastaus
-.then(function (responseJson){
-// Kutsutaan funktiota ja välitetään sille json-vastaus
-kerro(responseJson);
-})
-
-// Jos tuli jokin virhe
-.catch(function (error) {
-document.getElementById("vastaus").innerHTML =
-"<p>Tietoa ei pystytä hakemaan</p>";
-})
-
-function kerro (data){
-    //muuttuja tejsti, johon tulostettava tieto kerätään
-    var teksti="";
+function kerro(data) {
+    // Muuttuja, johon tulostettava tieto kerätään
+    var teksti = "";
     teksti = "<h1>" + data.otsikko + "</h1>";
     teksti = teksti + "<p>" + data.kuvaus + "</p>";
-    teksti = teksti + "<p><img src='" + data.kuva + "' alt='kuva' ></p>";
-    teksti = teksti + "<h3>Opintojakso: " + data.opintojakso.nimi + " " + data.opintojakso.tunnus + " " + data.opintojakso.opintopisteet + "op" + "</h3>";
+    teksti = teksti + "<p><img src='" + data.kuva + "' alt='kuva'></p>";
+    teksti =
+        teksti +
+        "<h3>Opintojakso: " +
+        data.opintojakso.nimi +
+        " " +
+        data.opintojakso.tunnus +
+        " " +
+        data.opintojakso.opintopisteet +
+        "op" +
+        "</h3>";
 
-    //taulukko
-    teksti = teksti + "<ul>"
-    // tauukon käsittely for-elementissä
-    for(var i = 0; i < data.sisalto.length; i++) {
+    // Taulukko
+    teksti = teksti + "<ul>";
+    // Taulukon käsittely for-silmukassa
+    for (var i = 0; i < data.sisalto.length; i++) {
         teksti = teksti + "<li>" + data.sisalto[i] + "</li>";
     }
-    //listaelementti kiinni
-    teksti = teksti + "</ul>"
+    teksti = teksti + "</ul>";
 
-    //objektitaulukko
+    // Objektitaulukko
     teksti = teksti + "<h3>Linkit</h3><ul>";
-    for(var i =0; i<data.tekniikat.lenght; i++){
-        teksti = teksti + "<li>" + data.tekniikat[i].aihe + " " + "<a href='data.tekniikat[i].linkki'>" + data.tekniikat[i].linkki + "</a></li>";
+    for (var i = 0; i < data.tekniikat.length; i++) {
+        teksti = teksti + "<li>" + data.tekniikat[i].aihe + " " + "<a href='" + data.tekniikat[i].linkki + "' target='_blank'>" + data.tekniikat[i].linkki + "</a></li>";
     }
+    teksti = teksti + "</ul>";
 
-
-//tulostus sivulle
-document.getElementById("vastaus").innerHTML = teksti;
+    // Tulostus sivulle
+    document.getElementById("vastaus").innerHTML = teksti;
 }
