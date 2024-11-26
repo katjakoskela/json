df55a0ea0dfb7c2115030b0c5af426f1eb663421
katjakoskela <159912923+katjakoskela@users.noreply.github.com>
2024-11-26 16:43:40 +0200

    js

---
+++ cafe.js
@@ -0,0 +1,19 @@
+fetch('https://raw.githubusercontent.com/katjakoskela/json/refs/heads/main/data.json')
+
+// Muunnetaan vastaus JSON muotoon
+.then(function (response) {
+return response.json();
+})
+
+// Käsitellään muunnettu (eli JSON muotoinen) vastaus
+.then(function (responseJson){
+// Kutsutaan funktiota ja välitetään sille json-vastaus
+kerro(responseJson);
+})
+
+// Jos tuli jokin virhe
+.catch(function (error) {
+document.getElementById("vastaus").innerHTML =
+"<p>Tietoa ei pystytä hakemaan</p>";
+})
+
