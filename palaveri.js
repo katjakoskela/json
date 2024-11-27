fa326a4250006259830cbb6653aa97bf26927eaa
katjakoskela <159912923+katjakoskela@users.noreply.github.com>
2024-11-27 18:42:58 +0200

    palaveri korjaus

--- palaveri.js
+++ palaveri.js
@@ -1,44 +1,39 @@
 fetch('https://raw.githubusercontent.com/katjakoskela/json/refs/heads/main/palaveri.json')
-
-// Muunnetaan vastaus JSON muotoon
-.then(function (response) {
-return response.json();
-})
-
-// Käsitellään muunnettu (eli JSON muotoinen) vastaus
-.then(function (responseJson){
-
-// Kutsutaan funktiota ja välitetään sille json-vastaus
-kerro(responseJson);
-})
-
-// Jos tuli jokin virhe
-.catch(function (error) {
-document.getElementById("vastaus").innerHTML =
-"<p>Tietoa ei pystytä hakemaan</p>";
-})
+  // Muunnetaan vastaus JSON muotoon
+  .then(function (response) {
+    return response.json();
+  })
+  // Käsitellään muunnettu (eli JSON muotoinen) vastaus
+  .then(function (responseJson) {
+    // Kutsutaan funktiota ja välitetään sille json-vastaus
+    kerro(responseJson);
+  })
+  // Jos tuli jokin virhe
+  .catch(function (error) {
+    document.getElementById("vastaus").innerHTML =
+      "<p>Tietoa ei pystytä hakemaan</p>";
+  });
 
 // Funktio tietojen näyttämiseksi
 function kerro(data) {
-    var teksti = "";
+  var teksti = "";
 
-    // Palaverin aihe
-    teksti = "<h1>" + data.aihe + "</h1>";
+  // Palaverin aihe
+  teksti = "<h1>" + data.aihe + "</h1>";
 
-    // Osallistujat
-    teksti = teksti + "<h3>Osallistujat (" + data.lukumaara + "):</h3><ul>";
-    for (var i = 0; i < data.nimet.length; i++) {
-        teksti = teksti + "<li>" + data.nimet[i] + "</li>";
-    }
-    teksti = teksti + "</ul>";
+  // Osallistujat
+  teksti = teksti + "<h3>Osallistujat (" + data.lukumaara + "):</h3><ul>";
+  for (var i = 0; i < data.nimet.length; i++) {
+    teksti = teksti + "<li>" + data.nimet[i] + "</li>";
+  }
+  teksti = teksti + "</ul>";
 
-    // Paikka ja aika
-    teksti = teksti + "<h3>Paikka ja aika</h3>";
-    teksti = teksti + "<p>Paikka: " + data.paikka + "</p>";
-    teksti = teksti + "<p>Alkamis aika: " + data.alkaminen + "</p>";
-    teksti = teksti + "<p>Kesto: " + data.kesto + "</p>";
+  // Paikka ja aika
+  teksti = teksti + "<h3>Paikka ja aika</h3>";
+  teksti = teksti + "<p>Paikka: " + data.paikka + "</p>";
+  teksti = teksti + "<p>Alkamis aika: " + data.alkaminen + "</p>";
+  teksti = teksti + "<p>Kesto: " + data.kesto + "</p>";
 
-    // Tulostus sivulle
-    document.getElementById("vastaus").innerHTML = teksti;
-
+  // Tulostus sivulle
+  document.getElementById("vastaus").innerHTML = teksti;
 }
