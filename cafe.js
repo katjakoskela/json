ef7885e7879d162f7bd62bea7e90aa992ffeeda6
katjakoskela <159912923+katjakoskela@users.noreply.github.com>
2024-11-27 17:17:34 +0200

    + pois

--- cafe.js
+++ cafe.js
@@ -22,24 +22,24 @@ function kerro(data) {
     let teksti = "";
 
     // Yhteystiedot
-    teksti += "<h1>" + data.yritys + "</h1>";
-    teksti += "<p>Osoite: " + data.yhteystiedot.osoite + "</p>";
-    teksti += "<p>Puhelin: " + data.yhteystiedot.puhelin + "</p>";
-    teksti += '<p>Email: <a href="mailto:' + data.yhteystiedot.email + '">' + data.yhteystiedot.email + "</a></p>";
+    teksti = "<h1>" + data.yritys + "</h1>";
+    teksti = "<p>Osoite: " + data.yhteystiedot.osoite + "</p>";
+    teksti = "<p>Puhelin: " + data.yhteystiedot.puhelin + "</p>";
+    teksti = '<p>Email: <a href="mailto:' + data.yhteystiedot.email + '">' + data.yhteystiedot.email + "</a></p>";
 
     // Tuotteet-taulukko
-    teksti += "<h3>Tuotteet</h3><ul>";
+    teksti = "<h3>Tuotteet</h3><ul>";
     for (let i = 0; i < data.tuotteet.length; i++) {
-        teksti += `<li>${data.tuotteet[i]}</li>`;
+        teksti = `<li>${data.tuotteet[i]}</li>`;
     }
-    teksti += "</ul>";
+    teksti = "</ul>";
 
     // Henkilökunta-taulukko
-    teksti += "<h3>Henkilökunta</h3><ul>";
+    teksti = "<h3>Henkilökunta</h3><ul>";
     for (let i = 0; i < data.henkilokunta.length; i++) {
-        teksti += `<li>${data.henkilokunta[i].nimi} - ${data.henkilokunta[i].titteli}</li>`;
+        teksti = `<li>${data.henkilokunta[i].nimi} - ${data.henkilokunta[i].titteli}</li>`;
     }
-    teksti += "</ul>";
+    teksti = "</ul>";
 
     // Tulostus sivulle
     document.getElementById("vastaus").innerHTML = teksti;
