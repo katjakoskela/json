4a93bda151d83739d3310e2ba3e207cf5281f3e8
katjakoskela <159912923+katjakoskela@users.noreply.github.com>
2024-11-27 18:08:15 +0200

    testi pois

--- palaveri.js
+++ palaveri.js
@@ -8,9 +8,8 @@ return response.json();
 // Käsitellään muunnettu (eli JSON muotoinen) vastaus
 .then(function (responseJson){
 // Testataan onnistuuko json-luku
-// jos onnistuu päivitetään tähän json-datan käsittelevän funktion kutsu
-document.getElementById("vastaus").innerHTML =
-"<p>Jatketaan harjoitusta</p>";
+// Kutsutaan funktiota ja välitetään sille json-vastaus
+kerro(responseJson);
 })
 
 // Jos tuli jokin virhe
@@ -20,26 +19,26 @@ document.getElementById("vastaus").innerHTML =
 })
 
 // Funktio tietojen näyttämiseksi
-//function kerro(data) {
-  //  var teksti = "";
+function kerro(data) {
+    var teksti = "";
 
     // Palaverin aihe
- //   teksti = "<h1>" + data.aihe + "</h1>";
+    teksti = "<h1>" + data.aihe + "</h1>";
 
     // Osallistujat
-   // teksti = teksti + "<h3>Osallistujat (" + data.lukumaara + "):</h3><ul>";
-    //for (var i = 0; i < data.nimet.length; i++) {
-      //  teksti = teksti + "<li>" + data.nimet[i] + "</li>";
-    //}
- //   teksti = teksti + "</ul>";
+    teksti = teksti + "<h3>Osallistujat (" + data.lukumaara + "):</h3><ul>";
+    for (var i = 0; i < data.nimet.length; i++) {
+        teksti = teksti + "<li>" + data.nimet[i] + "</li>";
+    }
+    teksti = teksti + "</ul>";
 
     // Paikka ja aika
-   // teksti = teksti + "<h3>Paikka ja aika</h3>";
- //   teksti = teksti + "<p>Paikka: " + data.paikka + "</p>";
- //   teksti = teksti + "<p>Alkamis aika: " + data.alkaminen + "</p>";
- //   teksti = teksti + "<p>Kesto: " + data.kesto + "</p>";
+    teksti = teksti + "<h3>Paikka ja aika</h3>";
+    teksti = teksti + "<p>Paikka: " + data.paikka + "</p>";
+    teksti = teksti + "<p>Alkamis aika: " + data.alkaminen + "</p>";
+    teksti = teksti + "<p>Kesto: " + data.kesto + "</p>";
 
     // Tulostus sivulle
-   // document.getElementById("vastaus").innerHTML = teksti;
+    document.getElementById("vastaus").innerHTML = teksti;
 
-//}
+}
