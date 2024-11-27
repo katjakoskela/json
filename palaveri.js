490b7a9ebb02f5298e14b18c3654c24aeba03015
katjakoskela <159912923+katjakoskela@users.noreply.github.com>
2024-11-27 19:02:58 +0200

    uusiks

--- palaveri.js
+++ palaveri.js
@@ -14,26 +14,26 @@ fetch('https://raw.githubusercontent.com/katjakoskela/json/refs/heads/main/palav
       "<p>Tietoa ei pystytä hakemaan</p>";
   });
 
-// Funktio tietojen näyttämiseksi
-function kerro(data) {
-  var teksti = "";
-
-  // Palaverin aihe
-  teksti = "<h1>" + data.aihe + "</h1>";
-
-  // Osallistujat
-  teksti = teksti + "<h3>Osallistujat (" + data.lukumaara + "):</h3><ul>";
-  for (var i = 0; i < data.nimet.length; i++) {
-    teksti += + "<li>" + data.nimet[i] + "</li>";
+  // funktio tietojen näyttämiseksi
+  function kerro(data) {
+    var teksti = "";
+  
+    // Palaverin aihe
+    teksti = "<h1>" + data.aihe + "</h1>";
+  
+    // Osallistujat
+    teksti = "<h3>Osallistujat (" + data.lukumaara + "):</h3><ul>";
+    for (var i = 0; i < data.nimet.length; i++) {
+      teksti += "<li>" + data.nimet[i] + "</li>";
+    }
+    teksti += "</ul>";
+  
+    // Paikka ja aika
+    teksti += "<h3>Paikka ja aika</h3>";
+    teksti += "<p>Paikka: " + data.paikka + "</p>";
+    teksti += "<p>Alkamis aika: " + data.alkaminen + "</p>";
+    teksti += "<p>Kesto: " + data.kesto + "</p>";
+  
+    // Tulostus sivulle
+    document.getElementById("vastaus").innerHTML = teksti;
   }
-  teksti += + "</ul>";
-
-  // Paikka ja aika
-  teksti += + "<h3>Paikka ja aika</h3>";
-  teksti += + "<p>Paikka: " + data.paikka + "</p>";
-  teksti += + "<p>Alkamis aika: " + data.alkaminen + "</p>";
-  teksti += + "<p>Kesto: " + data.kesto + "</p>";
-
-  // Tulostus sivulle
-  document.getElementById("vastaus").innerHTML = teksti;
-}
