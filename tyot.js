585aa5c2c6052896ec9935a2e447e5f7b8c8016d
katjakoskela <159912923+katjakoskela@users.noreply.github.com>
2024-11-29 09:05:09 +0200

    chatgpt

--- tyot.js
+++ tyot.js
@@ -1,29 +1,29 @@
-// Haetaan data REST API:sta
 fetch('http://gis.vantaa.fi/rest/tyopaikat/v1/Opetusala')
     .then(function (response) {
         return response.json(); // Muutetaan vastaus JSON-muotoon
     })
     .then(function (responseJson) {
-        // Kutsutaan funktiota joka näyttää tiedot
+        // Kutsutaan funktiota, joka näyttää tiedot
         kerro(responseJson);
     })
     .catch(function (error) {
         // Näytetään virheviesti, jos tietojen haku epäonnistuu
         document.getElementById("vastaus").innerHTML =
-            "<p>Tietoa ei pystytä hakemaan</p>";
+            "<p style='color: red;'>Tietoa ei pystytä hakemaan</p>";
     });
 
 // Funktio tietojen näyttämiseksi
 function kerro(data) {
-    var teksti = "";
-    teksti = "<ul>";
-    
-    // Luodaan lista
-    for (var i=0; i<data.lenght; i++){
-        teksti = teksti + "<li>" + data.tyotehtava[i]+ "</li>";
+    let teksti = "<h1>Vantaan kaupungin opetusalan avoimet työpaikat</h1>";
+    teksti += "<ul>";
+    for (let i = 0; i < data.length; i++) {
+        teksti += "<li>";
+        teksti += "<h3>" + data[i].tyotehtava + "</h3>";
+        teksti += "<p>" + data[i].osoite + "</p>";
+        teksti += "<p>Hakuaika päättyy: " + data[i].haku_paattyy_pvm + "</p>";
+        teksti += "<a href='" + data[i].linkki + "' target='_blank'>Lue lisää</a>";
+        teksti += "</li>";
     }
-    
-    teksti = teksti + "</ul>";
-    // Näytetään data sivulla
+    teksti += "</ul>";
     document.getElementById("vastaus").innerHTML = teksti;
 }
