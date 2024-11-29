746a7943e929f866162a2fb43864f04e8615359d
katjakoskela <159912923+katjakoskela@users.noreply.github.com>
2024-11-29 08:50:56 +0200

    chatgpt

--- tyot.js
+++ tyot.js
@@ -1,42 +1,34 @@
+// Haetaan data REST API:sta
 fetch('http://gis.vantaa.fi/rest/tyopaikat/v1/Opetusala')
-
-
-// Muunnetaan vastaus JSON muotoon
-.then(function (response) {
-return response.json();
-})
-
-// Käsitellään muunnettu (eli JSON muotoinen) vastaus
-.then(function (responseJson){
-// Testataan onnistuuko json-luku
-// jos onnistuu päivitetään tähän json-datan käsittelevän funktion kutsu
-document.getElementById("vastaus").innerHTML =
-"<p>Jatketaan harjoitusta</p>";
-})
-
-// Jos tuli jokin virhe
-.catch(function (error) {
-document.getElementById("vastaus").innerHTML =
-"<p>Tietoa ei pystytä hakemaan</p>";
-})
+    .then(function (response) {
+        return response.json(); // Muutetaan vastaus JSON-muotoon
+    })
+    .then(function (responseJson) {
+        // Kutsutaan funktiota joka näyttää tiedot
+        kerro(responseJson);
+    })
+    .catch(function (error) {
+        // Näytetään virheviesti, jos tietojen haku epäonnistuu
+        document.getElementById("vastaus").innerHTML =
+            "<p>Tietoa ei pystytä hakemaan</p>";
+    });
 
 // Funktio tietojen näyttämiseksi
 function kerro(data) {
-    var teksti = "";
-
-    // Otsikko
-    teksti = "<h1>Vantaan kaupungin opetusalan avoimet työpaikat</h1>";
+    let teksti = "<h1>Vantaan kaupungin opetusalan avoimet työpaikat</h1>";
     
     // Käydään läpi JSON-data ja luodaan lista
-            teksti = teksti + "<ul>";
-            for (var i = 0; i < data.length; i++) {
-                teksti = teksti + "<h3>" + data[i].tyotehtava + "</h3><br>";
-                teksti = teksti + "<p>" + data[i].osoite + "</p><br>";
-                teksti = teksti + "<a href='" + data[i].linkki + "' target='_blank'>Lue lisää</a>";
-            }
-            teksti = teksti + "</ul>";
-
-    // Tulostus sivulle
+    teksti += "<ul>";
+    for (let i = 0; i < data.length; i++) {
+        teksti += "<li>";
+        teksti += "<h3>" + data[i].tyotehtava + "</h3>";
+        teksti += "<p>" + data[i].osoite + "</p>";
+        teksti += "<p>Hakuaika päättyy: " + data[i].haku_paattyy_pvm + "</p>";
+        teksti += "<a href='" + data[i].linkki + "' target='_blank'>Lue lisää</a>";
+        teksti += "</li>";
+    }
+    teksti += "</ul>";
+    
+    // Näytetään data sivulla
     document.getElementById("vastaus").innerHTML = teksti;
-
 }
